42daa0a152d1fc3226e594d5abb6ef23575a1b95
Saqlain Rashid <saqlainrashed2001@gmail.com>
2023-07-30 00:55:47 +0530

    test 4

--- pages/freelanceItems.jsx
+++ pages/freelanceItems.jsx
@@ -3,11 +3,10 @@ import Link from 'next/link';
 import React from 'react';
 
 const FreelanceItem = ({ title, backgroundImg, tech, projectUrl }) => {
-  const auth = projectUrl?.auth || null;
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-      <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt={title} /> {/* Set alt text to title */}
-      <div className='hidden group-hover:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' />
+      <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
         <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
         <Link href={projectUrl}>
